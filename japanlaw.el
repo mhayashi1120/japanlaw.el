36382f5c1385b5dd28f436d4f4e54c94e59814ba
Masahiro Hayashi <mhayashi1120@gmail.com>
2015-09-09 22:38:39 +0900

    add doc

--- japanlaw.el
+++ japanlaw.el
@@ -31,7 +31,19 @@
 
 ;; ## Install:
 
-;;  w3m: http://w3m.sourceforge.net/index.ja.html
+;; Please install w3m command. (http://w3m.sourceforge.net/index.ja.html)
+;; Please install this package from MELPA. (http://melpa.milkbox.net/)
+
+;; Otherwise, put this file into load-path'ed directory, and byte compile it if
+;; desired. And put the following expression into your ~/.emacs.
+;;
+;;     (require 'japanlaw)
+
+;; ## Usage:
+
+;; * Quickstart
+
+;;     M-x japanlaw
 
 ;;; Code:
 
